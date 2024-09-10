b705d98b6ce1c5a48f0d1dc7ac057fffa7763fb9
ridarehmanmalik <ridarehmanmalik@gmail.com>
2024-09-10 14:37:07 +0500

    Add files via upload

---
+++ currency.py
@@ -0,0 +1,8 @@
+
+pesos=int(input('what do you left in pesos?'))
+soles=int(input('what do you left in soles?'))
+reais=int(input('what do you left in reais?'))
+
+total = pesos * 0.00025 + soles * 0.28 + reais * 0.21
+
+print(total)
